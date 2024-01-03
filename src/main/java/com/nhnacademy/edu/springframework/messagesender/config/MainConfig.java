3ed907a0c7ad663d8aa9bd4244190bc560e834b1
devhomh <jinwoo7654@naver.com>
2024-01-03 13:16:13 +0900

    Bean annotation 실습

--- src/main/java/com/nhnacademy/edu/springframework/messagesender/config/MainConfig.java
+++ src/main/java/com/nhnacademy/edu/springframework/messagesender/config/MainConfig.java
@@ -1,9 +1,18 @@
 package com.nhnacademy.edu.springframework.messagesender.config;
 
+import com.nhnacademy.edu.springframework.messagesender.annotation.SMS;
+import com.nhnacademy.edu.springframework.messagesender.message.MessageSender;
+import com.nhnacademy.edu.springframework.messagesender.message.SmsMessageSender;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @ImportResource("classpath:/beans.xml")
 public class MainConfig {
+    @Bean
+    @SMS
+    public MessageSender smsMessageSender() {
+        return new SmsMessageSender();
+    }
 }
